053b55e7d40bbe2e957fd690f33069053567fac0
mkstamin <mkstamincse@gmail.com>
2021-05-21 10:23:56 +0600

    Composition vs Inheritance

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import Calculator from './component/Calculator';
+import Bracket from './component/composition/Bracket';
+import Emoji from './component/composition/Emoji';
+import Text from './component/composition/Text';
 
 function App() {
-    return <Calculator />;
+    return (
+        <Emoji>
+            {/* {({ addEmoji }) => <Text addEmoji={addEmoji} />} */}
+            {({ addEmoji }) => (
+                <Bracket>
+                    {({ addBracket }) => <Text addEmoji={addEmoji} addBracket={addBracket} />}
+                </Bracket>
+            )}
+        </Emoji>
+    );
 }
 
 export default App;
