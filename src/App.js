dcf79023644d4ce0cf96705596ea79a2345234fe
mkstamin <mkstamincse@gmail.com>
2021-05-21 13:42:37 +0600

    Higher Order Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import Calculator from './component/Calculator';
+import ClickCounter from './component/ClickCounter';
+import HoverCounter from './component/HoverCounter';
 
 function App() {
-    return <Calculator />;
+    return (
+        <div>
+            <ClickCounter />
+            <HoverCounter />
+        </div>
+    );
 }
 
 export default App;
